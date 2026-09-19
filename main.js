c0d1f689b5ff67d11aa4fa375c40ba9b562d009c
sevennn-39 <sevenofspiders@gmail.com>
2026-09-19 23:02:33 +0000

    first commit!

---
+++ main.js
@@ -0,0 +1,7 @@
+let score = 0
+alert("The following is a version of the AX-CPT! The instructions are simple: press the space bar every time a letter apprear, UNLESS it's an X. Press OK to continue!")
+
+document.write("A")
+document.write("F")
+document.write("X")
+document.write("Y")
